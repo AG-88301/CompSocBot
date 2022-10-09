2f8a06cfe780bcd1b917d4ddf5e6806c59d9f5f4
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2022-10-09 19:41:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="?", intents=intents)
+
+@client.event
+async def on_ready() -> str:
+    print("Ready")
+
+client.run("Token")
