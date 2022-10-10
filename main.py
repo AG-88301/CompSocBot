143122276d96979cd1cc09137c3f0780eb5cb71f
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2022-10-10 19:34:08 +0100

    commit

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
 client = commands.Bot(command_prefix="?", intents=intents)
 
 @client.event
 async def on_ready() -> str:
     print("Ready")
 
-client.run("Token")
+@client.command() 
+async def update_roles(ctx):
+    for guild in client.guilds:
+        for member in guild.members:
+            member = member[:-5].split(sep=" / ")
+            
+            if not member.bot: await ctx.send(member)
+
+client.run(TOKEN)
